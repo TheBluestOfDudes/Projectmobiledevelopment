4430cb86ea9219fd8fe38c73c0e4524bef03d2fc
VegardSkaansar <vegaskaa.stud@ntnu.no>
2019-04-23 21:19:05 +0200

    Adding the fragments with their xml

---
+++ app/src/main/java/com/example/projectmobiledevelopment/DogListFragment.java
@@ -0,0 +1,35 @@
+package com.example.projectmobiledevelopment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DogListFragment extends Fragment {
+
+    private MainActivity mainActivity;
+
+    public DogListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_dog_list, container, false);
+
+        mainActivity = (MainActivity) getActivity();
+
+
+        return v;
+    }
+
+}
--- app/src/main/java/com/example/projectmobiledevelopment/MainActivity.java
+++ app/src/main/java/com/example/projectmobiledevelopment/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.projectmobiledevelopment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public FragmentManager fragmentManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+
+        // first boot up of app
+        if (savedInstanceState == null) {
+
+            //here we will place a fragment
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(new HomeFragment(), null)
+                    .commit();
+        }
     }
 }
