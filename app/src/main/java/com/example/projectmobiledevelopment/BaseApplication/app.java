447bf7fcb527bdbc567bf4a0747d23f95fee65ee
VegardSkaansar <vegaskaa.stud@ntnu.no>
2019-05-01 16:42:48 +0200

    Cleanup when app runs again delete all old dates, with cascade todoitem

--- app/src/main/java/com/example/projectmobiledevelopment/BaseApplication/app.java
+++ app/src/main/java/com/example/projectmobiledevelopment/BaseApplication/app.java
@@ -5,18 +5,27 @@ import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
+import android.os.SystemClock;
+
+import com.example.projectmobiledevelopment.Database.Database;
+import com.example.projectmobiledevelopment.Database.DatabaseWrapper;
+import com.example.projectmobiledevelopment.Database.dates;
+
+import java.util.Calendar;
+import java.util.List;
 
 
 public class app extends Application {
 
     public static final String CHANNEL_1_ID = "channel1";
-    public static final String CHANNEL_2_ID = "channel2";
+    private DatabaseWrapper db;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        db = new DatabaseWrapper(this, "Database");
         createNotificationChannels();
+        checkDBForOldDates();
 
     }
     private void createNotificationChannels() {
@@ -33,5 +42,20 @@ public class app extends Application {
         }
 
 
+    }
+
+    // doesnt check for all dates, or times but over time will be deleted
+    private void checkDBForOldDates() {
+        Calendar calendar = Calendar.getInstance();
+        List<dates> list = db.getDates();
+        for (dates date : list) {
+            if(date.getYear() < calendar.get(Calendar.YEAR)) {
+                db.deleteDate(date.getYear(), date.getMonth(), date.getDay());
+            } else if (date.getMonth() < calendar.get(Calendar.MONTH)) {
+                db.deleteDate(date.getYear(), date.getMonth(), date.getDay());
+            } else if (date.getMonth() == calendar.get(Calendar.MONTH) && date.getDay() < calendar.get(Calendar.DAY_OF_MONTH)){
+                db.deleteDate(date.getYear(), date.getMonth(), date.getDay());
+            }
+        }
     }
 }
